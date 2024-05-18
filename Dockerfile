593e17aef7a82e06a257577e26f6a66ba0bbb326
schneli3 <schneli3@students.zhaw.ch>
2024-05-18 12:08:44 +0200

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,11 @@ FROM openjdk:21-jdk-slim
 WORKDIR /usr/src/app
 COPY . .
 
+RUN chmod +x ./mvnw
+
 # Install
 RUN ./mvnw -Dmaven.test.skip=true package
 
 # Docker Run Command
-EXPOSE 8080
+EXPOSE 8082
 CMD ["java","-jar","/usr/src/app/target/playground-0.0.1-SNAPSHOT.jar"]
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,31 +1,47 @@
+function displayResults(data) {
+    const answerElement = document.getElementById('answer');
+    answerElement.innerHTML = '';
+
+    
+    try {
+        const jsonData = JSON.parse(data);
+        jsonData.forEach(item => {
+            const bar = document.createElement('div');
+            bar.style.width = (item.probability * 100) + '%';
+            bar.className = 'result-bar ' + item.className.toLowerCase();
+            bar.textContent = `${item.className}: ${(item.probability * 100).toFixed(2)}%`;
+            answerElement.appendChild(bar);
+        });
+    } catch (e) {
+        answerElement.textContent = 'Fehler beim Parsen der Antwort: ' + e.message;
+    }
+    document.getElementById('answerPart').style.visibility = 'visible';
+}
+
 function getSentiment(event, text) {
     console.log(text);
 
     if (!text || event.key !== "Enter") {
-        answer.innerHTML = '';
+        document.getElementById('answer').innerHTML = '';
+        document.getElementById('answerPart').style.visibility = "hidden";
         return;
     }
 
-    answerPart.style.visibility = "visible";
+    document.getElementById('answerPart').style.visibility = "visible";
 
-    // Get Sentiment
     fetch('/sentiment?' + new URLSearchParams({
         text: text,
     }), {
         method: 'GET',
         headers: {}
+    }).then(response => {
+        console.log(response)
+        response.text().then(function (text) {
+            displayResults(text);
+        });
     }).then(
-        response => {
-            console.log(response)
-            response.text().then(function (text) {
-                answer.innerHTML = text;
-            });
-
-        }
-    ).then(
         success => console.log(success)
     ).catch(
         error => console.log(error)
     );
-
 }
